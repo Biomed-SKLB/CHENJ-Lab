922b3a74e87392384029bab34a6f05b75288b5ca
Biomed-SKLB <140296258+Biomed-SKLB@users.noreply.github.com>
2026-07-22 13:04:32 +0800

    Prepare admin v2 member editor model

--- admin-v2/members.js
+++ admin-v2/members.js
@@ -1,9 +1,6 @@
 export function mergeMembers(baseMembers, overrides) {
     const map = new Map(overrides.map((item) => [item.base_member_key, item]));
-    return baseMembers.map((member) => ({
-        ...member,
-        ...(map.get(member.base_member_key) || {})
-    }));
+    return baseMembers.map((member) => ({ ...member, ...(map.get(member.base_member_key) || {}) }));
 }
 
 export function memberPayload(form) {
